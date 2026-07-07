b3b5858a4459ca03c7b4626e7f9f0c824971f85f
Deependra Pal <deependrap412@gmail.com>
2026-07-07 11:25:15 +0530

    feat: enhance cookie security settings and improve validation rules for username and email

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -34,7 +34,12 @@ const registerController = async (req, res) => {
       expiresIn: "7d",
     });
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     // 7. Response
     res.status(201).json({
@@ -85,7 +90,12 @@ const loginController = async (req, res) => {
     });
 
     // 6. Store Token in Cookie
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     // 7. Response
     res.status(200).json({
